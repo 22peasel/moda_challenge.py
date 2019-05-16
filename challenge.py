bafb729d2c1e4c7bb405d32c3a76f717bf0ae89c
22peasel <49483145+22peasel@users.noreply.github.com>
2019-05-16 12:01:14 -0400

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,21 @@
+print ("A")
+for x in range(10):
+    print("Hello, world!")
+y = 0
+print ("B")
+while y < 10:
+    print (y)
+    y = y + 1
+print ("C")
+z = 1
+while z < 11:
+    print (z)
+    z = z + 1
+print ("D")
+chad = 2
+while chad < 21:
+    print (chad)
+    chad = chad + 2
+print ("E")
+for i in range (1, 11):
+    print (i * i)
